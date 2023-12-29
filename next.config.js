545e4ec111f9e351861b21b8a25c1c0afb5d25c9
Harshit <gaurharshit4@gmail.com>
2023-12-30 01:03:46 +0530

    updated next config remote patters for img bug fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "localhost","https://marketplace-production-bc49.up.railway.app"
+    remotePatterns: [
+      {
+        hostname: "localhost",
+        pathname: "**",
+        protocol: "http",
+        port: "3000",
+      },
+      {
+        hostname: "marketplace-production-bc49.up.railway.app",
+        protocol: "https",
+      }
     ],
   },
+
 }
 
 module.exports = nextConfig
