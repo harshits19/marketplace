04bf8dab375a3170eb8ce5cfed78163965237e3c
Harshit <gaurharshit4@gmail.com>
2024-01-04 21:27:40 +0530

    middleware bug fix

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,14 @@
 import { NextRequest, NextResponse } from "next/server"
-import { useGetUser } from "./hooks/useGetUser"
+import { useGetUser } from "@/hooks/useGetUser"
 
 export async function middleware(req: NextRequest) {
   const { nextUrl, cookies } = req
   const { user } = await useGetUser(cookies)
-  if (!user?.id) {
-    console.log("no user")
+  if (!user) {
     if (nextUrl.pathname.startsWith("/wishlist") || nextUrl.pathname.startsWith("/orders")) {
       return NextResponse.redirect(`${process.env.NEXT_PUBLIC_SERVER_URL}/sign-in?origin=${nextUrl.pathname.slice(1)}`)
     }
   } else {
-    console.log("user", user)
     if (nextUrl.pathname.startsWith("/sign-in") || nextUrl.pathname.startsWith("/sign-up")) {
       if (nextUrl.search.includes("?origin=")) {
         const param = nextUrl.search.slice(8)
