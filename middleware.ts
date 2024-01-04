efb490a461a7ba01ef044b3acfe34fc733858125
Harshit <gaurharshit4@gmail.com>
2024-01-04 20:00:05 +0530

    middleware bug fix on production

--- middleware.ts
+++ middleware.ts
@@ -4,16 +4,18 @@ import { useGetUser } from "./hooks/useGetUser"
 export async function middleware(req: NextRequest) {
   const { nextUrl, cookies } = req
   const { user } = await useGetUser(cookies)
-  if (user) {
+  if (!user?.id) {
+    console.log("no user")
+    if (nextUrl.pathname.startsWith("/wishlist") || nextUrl.pathname.startsWith("/orders")) {
+      return NextResponse.redirect(`${process.env.NEXT_PUBLIC_SERVER_URL}/sign-in?origin=${nextUrl.pathname.slice(1)}`)
+    }
+  } else {
+    console.log("user", user)
     if (nextUrl.pathname.startsWith("/sign-in") || nextUrl.pathname.startsWith("/sign-up")) {
       if (nextUrl.search.includes("?origin=")) {
         const param = nextUrl.search.slice(8)
         return NextResponse.redirect(`${process.env.NEXT_PUBLIC_SERVER_URL}/${param}`)
       } else return NextResponse.redirect(`${process.env.NEXT_PUBLIC_SERVER_URL}/`)
-    }
-  } else {
-    if (nextUrl.pathname.startsWith("/wishlist") || nextUrl.pathname.startsWith("/orders")) {
-      return NextResponse.redirect(`${process.env.NEXT_PUBLIC_SERVER_URL}/sign-in?origin=${nextUrl.pathname.slice(1)}`)
     }
   }
   return NextResponse.next()
